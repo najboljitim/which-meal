a6120537a2113cc8561e911e040edc4f0c0b665c
najboljitim <59292609+najboljitim@users.noreply.github.com>
2019-12-31 17:44:59 +0100

    ceo sajt

--- which-meal/js/main.js
+++ which-meal/js/main.js
@@ -1,78 +1,55 @@
+(function() {
+  // navbar click
+  const bars = document.getElementsByClassName('bars')[0];
+  const navbar = document.getElementsByClassName('top-links')[0];
 
-(function(){
-        // navbar click
-        const bars = document.getElementsByClassName('bars')[0]
-        const navbar = document.getElementsByClassName('top-links')[0]
-        
-        
-        bars.addEventListener('click', function(){
+  bars.addEventListener('click', function() {
+    bars.classList.toggle('active');
+    navbar.classList.toggle('active');
+  });
 
-                bars.classList.toggle('active')
-                navbar.classList.toggle('active')
-  
-                
-        })
+  const form = document.getElementsByClassName('search-form')[0];
+  let inputField = document.getElementsByClassName('search-input')[0];
+  // vrednost inputa u console log
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    let input = inputField.value;
+    if (input === '') {
+      alert('Please fill input field properly');
+    }
+    console.log(input);
+    inputField.value = '';
+    inputField.focus();
+  });
 
+  const imageWrapper = document.getElementsByClassName('image-wrapper')[1];
+  const pTag = imageWrapper.getElementsByTagName('p');
+  const container = document.getElementsByClassName('flex-4')[0];
 
-        const form = document.getElementsByClassName('search-form')[0];
-        let inputField = document.getElementsByClassName('search-input')[0]
-        // vrednost inputa u console log
-        form.addEventListener('submit', function (e) {
-                e.preventDefault()
-                let input = inputField.value;
-                if(input === '' ){
-                        alert('Please fill input field properly')
-                }
-                console.log(input);
-                inputField.value = ''
-                inputField.focus();
-                
-        })
+  container.addEventListener('click', function(e) {
+    let clickedValue = e.target;
+    clickedValue = clickedValue.textContent.toLowerCase();
+    console.log(clickedValue);
 
+    let textP;
 
-        const imageWrapper = document.getElementsByClassName('image-wrapper')[1];
-        const pTag = imageWrapper.getElementsByTagName( 'p');
-        const container = document.getElementsByClassName('flex-4')[0]
+    for (let i = 0; i < pTag.length; i++) {
+      textP = pTag[i];
+      let textContentP = textP.textContent.toLowerCase();
 
-        
-        
-        container.addEventListener('click' , function (e) {
-        
-                let clickedValue = e.target
-                clickedValue = clickedValue.textContent.toLowerCase()
-                console.log(clickedValue);
+      if (clickedValue === textContentP) {
+        textP.classList.add('active');
+      } else {
+        textP.classList.remove('active');
+      }
 
-                let textP;
+      removingClassOfP(textP);
+    }
+  });
 
-                for(let i = 0; i< pTag.length; i++) {
-                        textP = pTag[i]
-                        let textContentP = textP.textContent.toLowerCase();
-
-                if(clickedValue === textContentP ) { 
-                        
-                        textP.classList.add('active');
-                        
-                } else{
-                        textP.classList.remove('active')
-                }
-                
-                removingClassOfP(textP)
-                        
-                } 
-
-        
-                
-        })
-
-        function removingClassOfP(pClass) { 
-
-                imageWrapper.addEventListener('click', function () {
-                        pClass.classList.remove('active')
-                })
-        }
-
-
-
-})()
-      
-
+  function removingClassOfP(pClass) {
+    imageWrapper.addEventListener('click', function() {
+      pClass.classList.remove('active');
+    });
+  }
+})();
